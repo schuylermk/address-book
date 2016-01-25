5cc0bb4b83361e6a1ac0f12a15c2d9626f43fad2
Alex Fallenstedt and Schuyler Klaassen <a.fallenstedt@gmail.com>
2016-01-25 11:25:15 -0800

    finish project

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,20 @@
 function Contact(firstName, lastName, address) {
   this.firstName = firstName;
   this.lastName = lastName;
-  this.address = [];
+  this.addresses = [];
 }
-
 Contact.prototype.fullName = function() {
  return this.firstName + " " + this.lastName;
 }
 
+function Address(street, city, state) {
+  this.street = street;
+  this.city = city;
+  this.state = state;
+}
+Address.prototype.fullAddress = function() {
+  return this.street + " " + this.city + ", " + this.state;
+}
 
 $(document).ready(function() {
   $("form#new-contact").submit(function(event) {
@@ -15,18 +22,21 @@ $(document).ready(function() {
 
     var inputtedFirstName = $("#new-first-name").val();//get variables
     var inputtedLastName = $("#new-last-name").val();
+    var inputtedStreet = $("#new-street").val();
     var inputtedCity = $("#new-city").val();
     var inputtedState = $("#new-state").val();
     var inputtedAddress = [];
 
-    var newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedAddress);
+    var newContact = new Contact(inputtedFirstName, inputtedLastName);
+    var newAddress = new Address(inputtedStreet, inputtedCity, inputtedState);
 
     $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");//appens contacts on contact list
 
-    $("input#new-first-name").val('');//These are converting our inputs into strings.
-    $("input#new-last-name").val('');//set
-    inputtedAddress.push($("input#new-city").val(''));
-    inputtedAddress.push($("input#new-state").val(''));
+    $("input#new-first-name").val('');//These reset fields after being submitted
+    $("input#new-last-name").val('');
+    $("input#new-street").val('');
+    $("input#new-city").val('');
+    $("input#new-state").val('');
 
 
     $(".contact").last().click(function(){//This shows the contact on the right side when it's clicked from the contact list
@@ -34,6 +44,7 @@ $(document).ready(function() {
       $("#show-contact h2").text(newContact.fullName());
       $(".first-name").text(newContact.firstName);
       $(".last-name").text(newContact.lastName);
+      $(".full-address").text(newAddress.fullAddress());
     });
   });
 });
--- specs/specs.js
+++ specs/specs.js
@@ -1,14 +1,25 @@
 describe ("Contact", function() {
-  it("creates contact with given properties", function() {
+  it("creates object for contact with given properties", function() {
     var myContact = new Contact("Seymour","Code");
     expect(myContact.firstName).to.equal("Seymour");
     expect(myContact.lastName).to.equal("Code");
-    expect(myContact.address).to.eql([]);
+
   });
   it("will add the fullName method for any contact", function() {
     var myContact = new Contact("Seymour","Code");
     expect(myContact.fullName()).to.equal("Seymour Code");
   });
-  
+});
 
+describe ("Address", function() {
+  it("will create an object for address properties", function() {
+    var myAddress = new Address("street", "city", "state");
+    expect(myAddress.street).to.equal("street");
+    expect(myAddress.city).to.equal("city");
+    expect(myAddress.state).to.equal("state");
+ });
+  it("will add the fullAddress method for any address", function() {
+    var myAddress = new Address("street", "city", "state");
+    expect(myAddress.fullAddress()).to.equal("street city, state");
+  });
 });
