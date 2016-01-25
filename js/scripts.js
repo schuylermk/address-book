ad93278ba0b905740c33d611703a80ad209a77dd
Alex Fallenstedt and Schuyler Klaassen <a.fallenstedt@gmail.com>
2016-01-25 10:07:20 -0800

    allow user to share first name and last name

--- js/scripts.js
+++ js/scripts.js
@@ -17,10 +17,16 @@ $(document).ready(function() {
     var inputtedLastName = $("#new-last-name").val();
     var newContact = new Contact(inputtedFirstName, inputtedLastName);
 
-    $("ul#contacts").append("<li><span class='contacts'>" + newContact.fullName() + "</span></li>");
+    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
 
     $("input#new-first-name").val('');
-   $("input#new-last-name").val('');
+    $("input#new-last-name").val('');
 
+    $(".contact").last().click(function(){
+      $("#show-contact").show();
+      $("#show-contact h2").text(newContact.fullName());
+      $(".first-name").text(newContact.firstName);
+      $(".last-name").text(newContact.lastName);
+    });
   });
 });
