7810ff8073c83209626703f66713adb50e673325
Alex Fallenstedt and Schuyler Klaassen <a.fallenstedt@gmail.com>
2016-01-25 08:49:13 -0800

    add first two specs

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,13 @@
+function Contact(firstName, lastName) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.address = [];
+}
+Contact.prototype.fullName = function() {
+ return this.firstName + " " + this.lastName; 
+}
+
+
 $(document).ready(function() {
-  
+
 });
---
+++ specs/specs.js
@@ -0,0 +1,13 @@
+describe ("Contact", function() {
+  it("creates contact with given properties", function() {
+    var myContact = new Contact("Seymour","Code");
+    expect(myContact.firstName).to.equal("Seymour");
+    expect(myContact.lastName).to.equal("Code");
+    expect(myContact.address).to.eql([]);
+  });
+  it("will add the fullName method for any contact", function() {
+    var myContact = new Contact("Seymour","Code");
+    expect(myContact.fullName()).to.equal("Seymour Code")
+  });
+
+});
